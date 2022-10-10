58abce8068b83f844be8df4e74805d1976cd3348
Lawrence Zhu <lawrence.zhu@coderclass.nl>
2022-10-10 12:24:38 +0200

    first commit, was kind of lazy to make it before

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private bool shootMode = false;
+    public float speed;
+    public GameObject bullet;
+    private Rigidbody rbBullet;
+    public GameObject barrel;
+    public float rotateSpeed = 5.0f;
+    private float rotx = 0.0f;
+    private float roty = 0.0f;
+    private float VertRotation;
+    private float HorizontalRotation;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rbBullet = bullet.GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate()
+    {
+        Debug.Log(shootMode);
+        if (shootMode == true)
+        {
+            if (Input.GetKeyDown("k"))
+            {
+                shootMode = false;
+            }
+            float RotateHorizontal = Input.GetAxis("Horizontal");
+            float RotateVertical = Input.GetAxis("Vertical");
+            rotx -= RotateHorizontal * rotateSpeed;
+            roty -= RotateVertical * rotateSpeed;
+            Debug.Log("rotx: " + rotx);
+            Debug.Log("roty: " + roty);
+            VertRotation -= rotx;
+            HorizontalRotation -= roty;
+            VertRotation = Mathf.Clamp(roty, -90.0f, 0.0f);
+            HorizontalRotation = Mathf.Clamp(rotx, -180.0f, 180.0f);
+
+
+            barrel.transform.eulerAngles = new Vector3 (VertRotation, -HorizontalRotation, 0.0f);
+        }
+
+        else if (shootMode == false)
+        {
+            if (Input.GetKeyDown("k"))
+            {
+                shootMode = true;
+            }
+            float MoveHorizontal = Input.GetAxis("Horizontal");
+            float MoveVertical = Input.GetAxis("Vertical");
+            Vector3 move = new Vector3(MoveHorizontal, 0.0f, MoveVertical);
+            rb.velocity = move * speed;
+        }
+    }
+}
+        /*if (shootMode = true)
+        {
+            float RotateHorizontal = Input.GetAxis("Horizontal");
+            float RotateVertical = Input.GetAxis("Vertical");
+            Quaternion rotation = Quaternion.Euler(RotateHorizontal, RotateVertical, 0);
+            Debug.Log(rotation);
+            barrel.transform.rotation = Quaternion.Slerp(barrel.transform.rotation, rotation, Time.deltaTime * rotateSpeed);
+
+        }*/
+        /*else if (shootMode = false)
+        {
+            Debug.Log("you are now out of shoot mode");
+            if (Input.GetKeyDown("j"))
+            {
+                shootMode = true;
+                Debug.Log("you are now out of shootmode");
+            }
+            Debug.Log("you are now playing");*/
+    //}
---
+++ Assets/Scripts/TileGeneration.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TileGeneration : MonoBehaviour {
+  [SerializeField]
+  NoiseGeneration noiseMapGeneration;
+  [SerializeField]
+  private MeshRenderer tileRenderer;
+  [SerializeField]
+  private MeshFilter meshFilter;
+  [SerializeField] 
+        private MeshCollider meshCollider;
+  [SerializeField]
+  private float mapScale;
+
+  [SerializeField]
+  private float heightMultiplier;
+
+  void Start() {
+    GenerateTile ();
+  }
+  void GenerateTile() {
+    Vector3[] meshVertices = this.meshFilter.mesh.vertices;
+    int tileDepth = (int)Mathf.Sqrt (meshVertices.Length);
+    int tileWidth = tileDepth;
+    float[,] heightMap = this.noiseMapGeneration.GenerateNoiseMap (tileDepth, tileWidth, this.mapScale);
+    Texture2D tileTexture = BuildTexture (heightMap);
+    this.tileRenderer.material.mainTexture = tileTexture;
+    UpdateMeshVertices(heightMap);
+  }
+  private Texture2D BuildTexture(float[,] heightMap) {
+    int tileDepth = heightMap.GetLength (0);
+    int tileWidth = heightMap.GetLength (1);
+    Color[] colorMap = new Color[tileDepth * tileWidth];
+    for (int zIndex = 0; zIndex < tileDepth; zIndex++) {
+      for (int xIndex = 0; xIndex < tileWidth; xIndex++) {
+        int colorIndex = zIndex * tileWidth+ xIndex;
+        float height= heightMap[zIndex, xIndex];
+        colorMap [colorIndex] = Color.Lerp (Color.black, Color.white, height);
+      }
+    }
+    Texture2D tileTexture = new Texture2D (tileWidth, tileDepth);
+    tileTexture.wrapMode = TextureWrapMode.Clamp;
+    tileTexture.SetPixels (colorMap);
+    tileTexture.Apply ();
+    return tileTexture;
+  }
+  
+  private void UpdateMeshVertices(float[,] heightMap) {
+    int tileDepth = heightMap.GetLength (0);
+    int tileWidth = heightMap.GetLength (1);
+    Vector3[] meshVertices = this.meshFilter.mesh.vertices;
+    int vertexIndex = 0;
+    for (int zIndex = 0; zIndex < tileDepth; zIndex++) {
+      for (int xIndex = 0; xIndex < tileWidth; xIndex++) {
+        float height = heightMap [zIndex, xIndex];
+        Vector3 vertex = meshVertices [vertexIndex];
+        meshVertices[vertexIndex] = new Vector3(vertex.x, height * this.heightMultiplier, vertex.z);
+        vertexIndex++;
+      }
+    }
+    this.meshFilter.mesh.vertices = meshVertices;
+    this.meshFilter.mesh.RecalculateBounds ();
+    this.meshFilter.mesh.RecalculateNormals ();
+    this.meshCollider.sharedMesh = this.meshFilter.mesh;
+  }
+}
