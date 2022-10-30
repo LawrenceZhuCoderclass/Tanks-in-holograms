b51289e8c4f0d6d0f920fdb5f92236a6836c914c
Lawrence Zhu <lawrence.zhu@coderclass.nl>
2022-10-30 22:10:10 +0100

    Added a game controller, there is now a turn based system where once a player shoots, the turn passes over

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject player_1;
+    public GameObject player_2;
+
+    private PlayerController player_1_script;
+    private PlayerController player_2_script;
+
+    private bool currentturn;
+    void Start()
+    {
+        player_1_script = player_1.GetComponent<PlayerController>();
+        player_2_script = player_2.GetComponent<PlayerController>(); 
+        currentturn = true;
+        //true means player-1's turn, false means player_2's turn
+    }
+    
+    public void NextTurn()
+    {
+        if (currentturn == true)
+        {
+            currentturn = false;
+            player_1_script.OwnTurn = false;
+            player_2_script.OwnTurn = true;
+        }
+        else if (currentturn == false)
+        {
+            currentturn = true;
+            player_1_script.OwnTurn = true;
+            player_2_script.OwnTurn = false;
+        }
+    } 
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,6 +20,10 @@ public class PlayerController : MonoBehaviour
     private float MoveHorizontal;
     private float MoveVertical;
     private bool IsGrounded = false;
+    public GameController Gamecontroller;
+    public bool OwnTurn;
+    private bool TouchOnce = true;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -29,48 +33,50 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        Debug.Log(shootMode);
-        if (shootMode == true)
+        if (OwnTurn == true)
         {
-            if (Input.GetKeyDown("k"))
+            if (shootMode == true)
             {
-                shootMode = false;
-                rb.isKinematic = false;
+                if (Input.GetKeyDown("k"))
+                {
+                    shootMode = false;
+                    rb.isKinematic = false;
+                }
+                float RotateHorizontal = Input.GetAxis("Horizontal");
+                float RotateVertical = Input.GetAxis("Vertical");
+                rotx -= RotateHorizontal * rotateSpeed;
+                roty -= RotateVertical * rotateSpeed;
+                VertRotation -= rotx;
+                HorizontalRotation -= roty;
+                VertRotation = Mathf.Clamp(roty, -90.0f, 0.0f);
+                HorizontalRotation = Mathf.Clamp(rotx, -180.0f, 180.0f);
+                barrel.transform.eulerAngles = new Vector3 (VertRotation, -HorizontalRotation, 0.0f);
             }
-            Debug.Log(rb.velocity);
-            float RotateHorizontal = Input.GetAxis("Horizontal");
-            float RotateVertical = Input.GetAxis("Vertical");
-            rotx -= RotateHorizontal * rotateSpeed;
-            roty -= RotateVertical * rotateSpeed;
-            VertRotation -= rotx;
-            HorizontalRotation -= roty;
-            VertRotation = Mathf.Clamp(roty, -90.0f, 0.0f);
-            HorizontalRotation = Mathf.Clamp(rotx, -180.0f, 180.0f);
-            barrel.transform.eulerAngles = new Vector3 (VertRotation, -HorizontalRotation, 0.0f);
+
+            else if (shootMode == false)
+            {         
+                MoveHorizontal = Input.GetAxis("Horizontal");
+                MoveVertical = Input.GetAxis("Vertical");
+                if (MoveHorizontal == 0.0f && MoveVertical == 0.0f && IsGrounded == true)
+                {
+                    rb.isKinematic = true;
+                }
+                else 
+                {
+                    rb.isKinematic = false;
+                }
+                if (Input.GetKeyDown("k"))
+                {
+                    shootMode = true;
+                    rb.isKinematic = true;
+                    MoveHorizontal = 0.0f;
+                    MoveVertical = 0.0f;
+                }  
+                Vector3 move = new Vector3(MoveHorizontal, -1.0f, MoveVertical);
+                rb.velocity = move * speed;
+            }
         }
 
-        else if (shootMode == false)
-        {         
-            MoveHorizontal = Input.GetAxis("Horizontal");
-            MoveVertical = Input.GetAxis("Vertical");
-            if (MoveHorizontal == 0.0f && MoveVertical == 0.0f && IsGrounded == true)
-            {
-                rb.isKinematic = true;
-            }
-            else
-            {
-                rb.isKinematic = false;
-            }
-            if (Input.GetKeyDown("k"))
-            {
-                shootMode = true;
-                rb.isKinematic = true;
-                MoveHorizontal = 0.0f;
-                MoveVertical = 0.0f;
-            }  
-            Vector3 move = new Vector3(MoveHorizontal, -1.0f, MoveVertical);
-            rb.velocity = move * speed;
-        }
     }
     void Update()
     {
@@ -78,16 +84,25 @@ public class PlayerController : MonoBehaviour
         {
             if (Input.GetKeyDown("l"))
             {
-                Debug.Log("trying to shoot");
                 Instantiate(bullet, hole.transform.position, Barrel_rotator.rotation);
+                shootMode = false;
+                rb.isKinematic = false;
+                Gamecontroller.NextTurn();
             }
         }
     }
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Ground")
-        {
-            IsGrounded = true;
-        }
+            if (collision.gameObject.tag == "Ground")
+            {
+                IsGrounded = true;              
+                if(this.gameObject.tag == "Player_2" && TouchOnce == true)
+                {
+                    OwnTurn = false;
+                    rb.isKinematic = true;
+                }
+                TouchOnce = false;
+            }
+
     }
 }
