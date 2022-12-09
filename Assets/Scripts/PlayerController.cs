f7875c5d12fb14ed268b3e97e5ba2bec6b8e0008
Lawrence Zhu <lawrence.zhu@coderclass.nl>
2022-12-09 15:43:21 +0100

    Cleaned the code for the 'PlayerController'. Now it uses Playerstates and functions with comments, instead of having no idea where what is when you first look at it

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,15 +8,19 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rbBullet;
 
     public bool shootMode = false;
+    public bool OwnTurn;
+    private bool TouchOnce = true;
+    private bool IsGrounded = false;
 
     public float speed;
-
-    public GameObject bullet;
-    public GameObject barrelRotator;
-    public GameObject barrel;
-    public GameObject hole;
-    //public GameObject cameraRotator;
-
+    private float mirrorcontrol;
+    public float Fuel = 5.0f;
+    private float BrunRate = 1.0f;
+    public float hp;
+    public float startHP = 10.0f;
+    public float beginPower;
+    private float maxPower;
+    public float power;
     public float rotateSpeed = 5.0f;
     private float rotx = 0.0f;
     private float roty = 0.0f;
@@ -25,17 +29,14 @@ public class PlayerController : MonoBehaviour
     private float CorrectMoveX;
     private float CorrectMoveZ;
     public float cameraAngle;
-    private bool IsGrounded = false;
+
+    public GameObject bullet;
+    public GameObject barrelRotator;
+    public GameObject barrel;
+    public GameObject hole;
+
     public GameController Gamecontroller;
-    public bool OwnTurn;
-    private bool TouchOnce = true;
-    public float Fuel = 5.0f;
-    private float BrunRate = 1.0f;
-    public float hp;
-    public float startHP = 10.0f;
-    public float beginPower;
-    private float maxPower;
-    public float power;
+
     public string otherPlayer;
     private string playerXInput;
     private string playerYInput;
@@ -43,8 +44,16 @@ public class PlayerController : MonoBehaviour
     private string shootInput;
     private string posPowerInput;
     private string negPowerInput;
-    private float mirrorcontrol;
+    
     public playerText statsText;
+    
+    private PlayerState playerState;
+
+    public enum PlayerState
+    {
+        Driving,
+        Shooting
+    }
 
     void Start()
     {
@@ -91,67 +100,15 @@ public class PlayerController : MonoBehaviour
     {
         if (OwnTurn == true)
         {
-            if (shootMode == true)
+            switch (playerState)
             {
-                //if (Input.GetKeyDown("k"))
-                //{
-                //    shootMode = false;
-                //    rb.constraints = RigidbodyConstraints.None;
-                //}
-                float RotateHorizontal = mirrorcontrol * Input.GetAxis(playerXInput);
-                float RotateVertical = Input.GetAxis(playerYInput);
-                rotx -= RotateHorizontal * rotateSpeed;
-                roty -= RotateVertical * rotateSpeed;
+                case PlayerState.Driving:
+                    Drive();
+                    break;
+                case PlayerState.Shooting:
+                    RotateBarrel();
+                    break;
 
-                roty = Mathf.Clamp(roty, -40.0f, 90.0f);
-                barrelRotator.transform.eulerAngles = new Vector3(-roty, -rotx, 0.0f);
-
-                float PosChangePower = 1.5f * Input.GetAxis(posPowerInput);
-                float NegChangePower = 1.5f * Input.GetAxis(negPowerInput);
-                power += PosChangePower - NegChangePower;
-                power = Mathf.Clamp(power, 0, maxPower);
-            }
-
-            else if (shootMode == false)
-            {
-                //if (Gamecontroller.mirrorControls) { MoveHorizontal = -Input.GetAxis(playerXInput); }
-                //else { MoveHorizontal = Input.GetAxis(playerXInput); }
-                MoveHorizontal = mirrorcontrol * Input.GetAxis(playerXInput);
-                MoveVertical = Input.GetAxis(playerYInput);
-
-                if (MoveHorizontal == 0.0f && MoveVertical == 0.0f && IsGrounded == true)
-                {
-                    rb.constraints = RigidbodyConstraints.FreezePosition;
-                }
-                else
-                {
-                    rb.constraints = RigidbodyConstraints.None;
-                }
-                if (MoveHorizontal != 0.0f || MoveVertical != 0.0f)
-                {
-                    Fuel -= BrunRate * Time.deltaTime;
-                }
-                //if (Input.GetKeyDown("k"))
-                //{
-                //    shootMode = true;
-                //    rb.constraints = RigidbodyConstraints.FreezePosition;
-                //    MoveHorizontal = 0.0f;
-                //    MoveVertical = 0.0f;
-                //}
-                if (Fuel > 0.0f)
-                {
-                    //cameraAngle = (cameraRotator.transform.eulerAngles.y) * Mathf.Deg2Rad;
-                    CorrectMoveX = MoveHorizontal * Mathf.Cos(cameraAngle) + MoveVertical * Mathf.Sin(cameraAngle);
-                    CorrectMoveZ = MoveVertical * Mathf.Cos(cameraAngle) - MoveHorizontal * Mathf.Sin(cameraAngle);
-
-                    Vector3 move = new Vector3(CorrectMoveX, -1.0f, CorrectMoveZ);
-                    rb.velocity = move * speed;
-                }
-                else
-                {
-                    rb.constraints = RigidbodyConstraints.FreezePosition;
-                    Fuel = 0;
-                }
             }
         }
         else
@@ -161,54 +118,31 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    public void TakeDamage(float damage)
-    {
-        hp = hp - damage;
-        if (hp <= 0)
-        {
-            Gamecontroller.Winner_Declaration(otherPlayer);
-        }
-        maxPower = beginPower * hp / startHP;
-        power = Mathf.Clamp(power, 0, maxPower);
-    }
 
     void Update()
     {
         if (OwnTurn == true)
         {
-            if (shootMode == true)
+            switch(playerState)
             {
-                if (Input.GetButtonDown(shootInput))
-                {
-                    GameObject shotBullet = Instantiate(bullet, hole.transform.position, barrel.transform.rotation);
-                    shotBullet.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, power, 0));
-                    //shootMode = false;
-                    rb.constraints = RigidbodyConstraints.FreezePosition;
-                    OwnTurn = false;
-                    statsText.ownTurn = false;
-                    statsText.noText();
-                    //Gamecontroller.NextTurn();
-                }
-                if (Input.GetButtonDown(modeInput))
-                {
-                    shootMode = false;
-                    rb.constraints = RigidbodyConstraints.None;
-                    statsText.shootMode = false;
-                }
-            }
-            else
-            {
-                if (Input.GetButtonDown(modeInput))
-                {
-                    shootMode = true;
-                    rb.constraints = RigidbodyConstraints.FreezePosition;
-                    MoveHorizontal = 0.0f;
-                    MoveVertical = 0.0f;
-                    statsText.shootMode = true;
-                }
+                case PlayerState.Driving:
+                    if (Input.GetButtonDown(modeInput))
+                    {
+                        playerState = PlayerState.Shooting;
+                        rb.constraints = RigidbodyConstraints.FreezePosition;
+                        MoveHorizontal = 0.0f;
+                        MoveVertical = 0.0f;
+                        statsText.shootMode = true;
+                    }
+                    break;
+                case PlayerState.Shooting:
+                    ShootBullet();
+                    break;
+
             }
         }
     }
+    //--------------------------------------Touching the Ground Function--------------------------------------
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Ground")
@@ -231,5 +165,84 @@ public class PlayerController : MonoBehaviour
             }
             TouchOnce = false;
         }
+    }
+    //--------------------------------------Driving Function--------------------------------------
+    void Drive()
+    {
+        MoveHorizontal = mirrorcontrol * Input.GetAxis(playerXInput);
+        MoveVertical = Input.GetAxis(playerYInput);
+
+        if (MoveHorizontal == 0.0f && MoveVertical == 0.0f && IsGrounded == true)
+        {
+            rb.constraints = RigidbodyConstraints.FreezePosition;
+        }
+        else
+        {
+            rb.constraints = RigidbodyConstraints.None;
+        }
+        if (MoveHorizontal != 0.0f || MoveVertical != 0.0f)
+        {
+            Fuel -= BrunRate * Time.deltaTime;
+        }
+
+        if (Fuel > 0.0f)
+        {
+            CorrectMoveX = MoveHorizontal * Mathf.Cos(cameraAngle) + MoveVertical * Mathf.Sin(cameraAngle);
+            CorrectMoveZ = MoveVertical * Mathf.Cos(cameraAngle) - MoveHorizontal * Mathf.Sin(cameraAngle);
+
+            Vector3 move = new Vector3(CorrectMoveX, -1.0f, CorrectMoveZ);
+            rb.velocity = move * speed;
+        }
+        else
+        {
+            rb.constraints = RigidbodyConstraints.FreezePosition;
+            Fuel = 0;
+        }
+    }
+    //--------------------------------------Shooting Function--------------------------------------
+    void ShootBullet()
+    {
+        if (Input.GetButtonDown(shootInput))
+        {
+            GameObject shotBullet = Instantiate(bullet, hole.transform.position, barrel.transform.rotation);
+            shotBullet.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, power, 0));
+            rb.constraints = RigidbodyConstraints.FreezePosition;
+            OwnTurn = false;
+            statsText.ownTurn = false;
+            statsText.noText();
+        }
+        if (Input.GetButtonDown(modeInput))
+        {
+            playerState = PlayerState.Driving;
+            rb.constraints = RigidbodyConstraints.None;
+            statsText.shootMode = false;
+        }
+    }
+    //--------------------------------------Barrel Rotation Function--------------------------------------
+    void RotateBarrel()
+    {
+        float RotateHorizontal = mirrorcontrol * Input.GetAxis(playerXInput);
+        float RotateVertical = Input.GetAxis(playerYInput);
+        rotx -= RotateHorizontal * rotateSpeed;
+        roty -= RotateVertical * rotateSpeed;
+
+        roty = Mathf.Clamp(roty, -40.0f, 90.0f);
+        barrelRotator.transform.eulerAngles = new Vector3(-roty, -rotx, 0.0f);
+
+        float PosChangePower = 1.5f * Input.GetAxis(posPowerInput);
+        float NegChangePower = 1.5f * Input.GetAxis(negPowerInput);
+        power += PosChangePower - NegChangePower;
+        power = Mathf.Clamp(power, 0, maxPower);
+    }
+    //--------------------------------------Taking Damage Function--------------------------------------
+    public void TakeDamage(float damage)
+    {
+        hp = hp - damage;
+        if (hp <= 0)
+        {
+            Gamecontroller.Winner_Declaration(otherPlayer);
+        }
+        maxPower = beginPower * hp / startHP;
+        power = Mathf.Clamp(power, 0, maxPower);
     }
 }
